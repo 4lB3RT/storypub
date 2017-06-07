53ac0c90a1ec44e93a5d801eee0b4ca5fd919260
4LB3RT <albertgarcia.agc@gmail.com>
2017-06-07 20:14:34 +0200

    Documentation class db

--- sys/db.php
+++ sys/db.php
@@ -6,11 +6,17 @@
 		static $instance;
 
 		public function __construct(){
+
+		    //get instance from registry
 			$config=Registry::getInstance();
+
+			//make array with configuration from config.json
 			$dbconf=(array)$config->dbconf;
 			$dsn=$dbconf['driver'].':host='.$dbconf['dbhost'].';dbname='.$dbconf['dbname'];
 		 	$usr=$dbconf['dbuser'];
 		 	$pwd=$dbconf['dbpass'];
+
+		 	//pass to config to pdo
 			parent::__construct($dsn,$usr,$pwd);
 		}
 
